9a33c839251cf683198007638439a7ce462a50b8
Miranda Luo <mluo20@pascack.org>
2018-01-13 15:20:27 -0500

    stopwatch

--- js/ui.js
+++ js/ui.js
@@ -45,6 +45,54 @@ function addReview() {
     }
 }
 
+/**STOPWATCH**/
+
+
+var Example1 = new (function() {
+
+    // Stopwatch element on the page
+    var $stopwatch;
+    
+    // Timer speed in milliseconds
+    var incrementTime = 100;
+
+    // Current timer position in milliseconds
+    var currentTime = 0;
+    
+    // Start the timer
+    $(function() {
+        $stopwatch = $('#stopwatch');
+        Example1.Timer = $.timer(updateTimer, incrementTime, false);  
+    });
+
+    // Output time and increment
+    function updateTimer() {
+        var timeString = formatTime(currentTime);
+        $stopwatch.html(timeString);
+        currentTime += incrementTime;
+    }
+
+    // Reset timer
+    this.resetStopwatch = function() {
+        currentTime = 0;
+        Example1.Timer.stop().once();
+    };
+
+});
+
+function pad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {str = '0' + str;}
+    return str;
+}
+function formatTime(time) {
+    time = time / 10;
+    var min = parseInt(time / 6000),
+        sec = parseInt(time / 100) - (min * 60),
+        hundredths = pad(time - (sec * 100) - (min * 6000), 2);
+    return (min > 0 ? pad(min, 2) : "00") + ":" + pad(sec, 2) + ":" + hundredths;
+}
+
 /**FORM HANDLING**/
 
 var scoutingvalues = document.forms["scoutingform"];

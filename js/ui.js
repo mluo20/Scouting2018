cb1b076c865e5e73f835b5ccc08840b18f51d6cd
mluo20 <mluo20@pascack.org>
2018-01-14 21:06:18 -0500

    (Almost) finished review page

--- js/ui.js
+++ js/ui.js
@@ -92,15 +92,19 @@ var scoutingvalues = document.forms["scoutingform"];
 function toggleForm(option) {
 	if (option == scoutingvalues["switch1"]) {
 		document.getElementById("correctside1").style.display = "block";
+        document.getElementById("c1").style.display = "block";
 	}
 	else if (option == scoutingvalues["switch2"]) {
 		document.getElementById("correctside1").style.display = "none";
+        document.getElementById("c1").style.display = "none";
 	}
 	else if (option == scoutingvalues["scale1"]) {
 		document.getElementById("correctside2").style.display = "block";
+        document.getElementById("c2").style.display = "block";
 	}
 	else if (option == scoutingvalues["scale2"]) {
 		document.getElementById("correctside2").style.display = "none";
+        document.getElementById("c2").style.display = "none";
 	}
 }
 
@@ -126,7 +130,13 @@ function increment(element) {
 
 $(function() {
 
-    $('#scoutingform').on('keyup keypress', function(e) {
+    var lis = $("li");
+    $("#prematchvals").find(lis).append("<span></span>");
+    $("#autonvals").find(lis).append("<span></span>");
+    $("#teleopvals").find(lis).append("<span></span>");
+    $("#postmatchvals").find(lis).append("<span></span>");
+
+    $('#scoutingform').on('keyup keypress change mouseup', function(e) {
 
         var keyCode = e.keyCode || e.which;
         if (keyCode === 13) { 
@@ -134,11 +144,48 @@ $(function() {
             return false;
         }
 
-        $("#prematchvals").find("span:first-child").text(scoutingvalues["matchnum"].value);
-        // $("#prematchvals").find("li")[1].append(scoutingvalues['teamnum'].value);
-        // $("#prematchvals").find("li")[2].append(scoutingvalues['alliance'].value);
+        var prematchspans = $("#prematchvals").find("span");
+        prematchspans.eq(0).text(scoutingvalues["matchnum"].value);
+        prematchspans.eq(1).text(scoutingvalues['teamnum'].value);
+        prematchspans.eq(2).text(scoutingvalues['alliance'].value);
+
+        var autonspans = $("#autonvals").find("span");
+        var baseline = scoutingvalues["baseline"].value == 0 ? "no" : "yes";
+        var switch1 = scoutingvalues["switch"].value == 0 ? "no" : "yes";
+        var correctswitch = scoutingvalues["correctswitch"].value == 0 ? "no" : "yes";
+        var scale = scoutingvalues["scale"].value == 0 ? "no" : "yes";
+        var correctscale = scoutingvalues["correctscale"].value == 0 ? "no" : "yes";
+        autonspans.eq(0).text(baseline);
+        autonspans.eq(1).text(switch1);
+        autonspans.eq(2).text(correctswitch);
+        autonspans.eq(3).text(scale);
+        autonspans.eq(4).text(correctscale);
+
+        var teleopspans = $("#teleopvals").find("span");
+        teleopspans.eq(0).text(scoutingvalues["portalcubes"].value);
+        teleopspans.eq(1).text(scoutingvalues["exchangecubes"].value);
+        teleopspans.eq(2).text(scoutingvalues["groundcubes"].value);
+
+        var postmatchspans = $("#postmatchvals").find("span");
+        var climbvalues = "";
+        for (var i = 0; i < scoutingvalues["climbing[]"].length; i++) {
+            if (scoutingvalues["climbing[]"][i].checked) {
+                climbvalues += scoutingvalues["climbing[]"][i].value + " ";
+            }
+        }
+        postmatchspans.eq(0).text(climbvalues);
+        postmatchspans.eq(1).text(scoutingvalues["bluescore"].value);
+        postmatchspans.eq(2).text(scoutingvalues["redscore"].value);
 
     });
+
+    // $('.nextlink').keyup(function(e) {
+    //     var keyCode = e.keyCode || e.which;
+    //     if (keyCode === 39) {
+    //         nextPage("auton");
+    //     }
+    //     console.log("hello");
+    // });
 
 });
 

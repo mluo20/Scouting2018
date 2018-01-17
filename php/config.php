21569f926b68b1bb0b7b07aa18a18042aa243f86
mluo20 <mluo20@pascack.org>
2018-01-17 09:03:09 -0500

    changed config

--- php/config.php
+++ php/config.php
@@ -1,2 +1,13 @@
 <?php
 
+define("SERVER", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE", "Scouting2018");
+
+connect() {
+	return new mysqli(SERVER, USERNAME, PASSWORD, DATABASE);
+	// if ($conn->connect_error) {
+ //    	die("Connection failed: " . $conn->connect_error);
+	// } 
+}
